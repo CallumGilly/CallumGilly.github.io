f08126f81a884816a266218fdefdf23803a962f9
CallumGilly <43007634+CallumGilly@users.noreply.github.com>
2020-11-04 17:56:14 +0000

    Why did I decide to make flappy bird again????

--- homeProjects/FlappyBird/script.js
+++ homeProjects/FlappyBird/script.js
@@ -46,25 +46,56 @@ class birdy {
   }
   jump() {
     this.upVelocity = 1.25;
-    this.speed = -1.8;
+    this.speed = -1.2;
   }
 }
 class pipe {
-  constructor (position) {
-    this.x = 1200 + (100 * position)
+  constructor (position, fX, fY) {
+    this.frameX = fX;
+    this.frameY = fY;
+    this.x = this.frameX + (400 * position);
+    this.height = null;
+    this.space = null;
+    this.newHeight();
+  }
+  newHeight () {
+    var proposedHeight = 0;
+    do {
+      proposedHeight = Math.floor(Math.random()*400);
+      console.log(proposedHeight);
+    }
+    while(!(proposedHeight < 100 || proposedHeight > this.frameY - 200));
+    this.height = proposedHeight;
+    this.space = (Math.floor(Math.random() * 60)+50)
+  }
+  drawPipe() {
+    fill(0,255,30);
+    rect(this.x,0,50,this.height);
+    rect(this.x,this.height + this.space,50,this.frameY);
+  }
+  update() {
+    this.x -= 1;
   }
 }
 
 var bird;
+var pipes = [];
 function setup() {
   createCanvas(1200, 400);
   bird = new birdy();
+  for (var ai = 0; ai < 5; ai++) {
+    pipes.push(new pipe(ai,1200,400));
+  }
 }
 
 function draw() {
   background(220);
   bird.speeds();
   bird.draw();
+  for (var ai = 0; ai < pipes.length; ai++) {
+    pipes[ai].update();
+    pipes[ai].drawPipe();
+  }
 }
 function keyPressed() {
   if (keyCode == 32) {
