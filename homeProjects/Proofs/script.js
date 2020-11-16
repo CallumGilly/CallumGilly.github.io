a1d7a760c9c664da2b7db0753563ffdc4f8daf2e
CallumGilly <43007634+CallumGilly@users.noreply.github.com>
2020-11-16 16:15:43 +0000

    updates to script

--- homeProjects/Proofs/script.js
+++ homeProjects/Proofs/script.js
@@ -1,6 +1,14 @@
-var points  = [];
-
+/*jshint esversion: 6 */
+var shape;
+class shapeClass {
+  constructor(formObject) {
+    this.a = [formObject[0].value,formObject[1].value];
+    this.b = [formObject[2].value,formObject[3].value];
+    this.c = [formObject[4].value,formObject[5].value];
+    this.d = [formObject[6].value,formObject[7].value];
+  }
+}
 function rombusSubmit() {
-  var shape = documents.forms.Rombus;
-  console.log(shape);
+  shape = new shapeClass(document.forms.Rombus);
+  return false;
 }
