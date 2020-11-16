67bb881c5a750f888a8941791d07253ed603668d
CallumGilly <43007634+CallumGilly@users.noreply.github.com>
2020-11-16 20:19:50 +0000

    Added more to scripts

--- homeProjects/Proofs/script.js
+++ homeProjects/Proofs/script.js
@@ -1,14 +1,15 @@
 /*jshint esversion: 6 */
 var shape;
 class shapeClass {
-  constructor(formObject) {
-    this.a = [formObject[0].value,formObject[1].value];
-    this.b = [formObject[2].value,formObject[3].value];
-    this.c = [formObject[4].value,formObject[5].value];
-    this.d = [formObject[6].value,formObject[7].value];
+  constructor(formObject, points) {
+    this.points = [];
+    for (var x = 0; x < points*2; x = x + 2) {
+      this.points.push([formObject[x].value,formObject[x+1].value]);
+    }
   }
+
 }
 function rombusSubmit() {
-  shape = new shapeClass(document.forms.Rombus);
+  shape = new shapeClass(document.forms.Rombus,4);
   return false;
 }
